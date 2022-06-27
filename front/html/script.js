1f47d74079a7a5d2201ae5b3cde970927e0a682b
Anthony Bano <anthony.bano.34@gmail.com>
2022-06-27 19:28:08 +0200

    a

--- front/html/script.js
+++ front/html/script.js
@@ -3,7 +3,7 @@ fetch("http://localhost:3000/api/products")
 .then(reponse => reponse.json())
 .then(data => 
     {   
-        while(console.log(data.length)>=0){
+        while(console.log(data.length)>0){
         let valuelenght=console.log(data.lenght)-1;
         console.log(data[valuelenght].imageUrl) }
     }
