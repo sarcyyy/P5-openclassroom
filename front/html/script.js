9d165e7b5efb979f83a5c0fa24d991c79c5db67c
Anthony Bano <anthony.bano.34@gmail.com>
2022-06-27 20:52:54 +0200

    a

--- front/html/script.js
+++ front/html/script.js
@@ -1,10 +1,50 @@
 
 fetch("http://localhost:3000/api/products")
 .then(reponse => reponse.json())
-.then(data => 
+.then(data => {
+
+    data.forEach(function(data) {
+        console.log(data.description);
+        console.log(data.name);
+        console.log(data.imageUrl);
+        let description = data.description;
+        const element = document.getElementById("zonetexte");
+        element.innerHTML = description;
+        const img = document.getElementById("zoneimg");
+        img.src= data.imageUrl;
+        const nom = document.getElementById("nom");
+        nom.innerHTML = data.name;
+        
+   }
+   )
+});
+
+/* lier le while et utiliser fonction recherche dans ID pour créer nb case */
+
+
+/*.then(data => 
     {   
         while(console.log(data.length)>0){
-        let valuelenght=console.log(data.lenght)-1;
-        console.log(data[valuelenght].imageUrl) }
+        let valuelenght =console.log(data.lenght)-1;
+        let urlfinale = console.log(data[valuelenght].imageUrl) }
     }
+  
+)*/
+
+/*.then(data => {
+    let dataLength = data.length;
+    let urlid = data.imageUrl;
+    while (dataLength>0){
+        dataLength = dataLength - 1;
+        urlid= data.imageUrl;
+        console.log(urlid);
+    }
+
+
+    console.log(dataLength);
+}
 )
+.catch(error => console.log(error));
+*/
+
+
