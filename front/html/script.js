0e01a32f3528d4c705baffd6b47f31ca18264d38
Anthony Bano <anthony.bano.34@gmail.com>
2022-06-28 12:23:59 +0200

    a

--- front/html/script.js
+++ front/html/script.js
@@ -1,20 +1,33 @@
+function Nouvellecard(image, nom, description){
+    fetch("http://localhost:3000/api/products")
+    .then(reponse => reponse.json())
+    .then( data => {
+        const Nouvellecard = document.createElement("a");
+        Nouvellecard.classList.add("items");
+        Nouvellecard.innerHTML = `<img src="${image}"><h3>${nom}</h3><p>${description}</p>`;
+        document.getElementById("items").appendChild(Nouvellecard);
+    })
+
+}
+
 
 fetch("http://localhost:3000/api/products")
 .then(reponse => reponse.json())
-.then(data => {
+.then( data => {
 
     data.forEach(function(data) {
         console.log(data.description);
         console.log(data.name);
         console.log(data.imageUrl);
-        let description = data.description;
-        const element = document.getElementById("zonetexte");
-        element.innerHTML = description;
-        const img = document.getElementById("zoneimg");
-        img.src= data.imageUrl;
-        const nom = document.getElementById("nom");
-        nom.innerHTML = data.name;
-        
+        // let description = data.description;
+        // const element = document.getElementById("zonetexte");
+        // element.innerHTML = description;
+        // const img = document.getElementById("zoneimg");
+        // img.src= data.imageUrl;
+        // const nom = document.getElementById("nom");
+        // nom.innerHTML = data.name;
+        Nouvellecard(`${data.imageUrl}`,`${data.name}`,`${data.description}`);
+    
    }
    )
 });
